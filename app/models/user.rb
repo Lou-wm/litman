a22f7eec139941fc40da49d860902f7d7ce52ecf
Lou <2635501727@qq.com>
2021-08-13 19:23:16 +0800

    validataes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, length: { maximum: 255 },
-  validates :password , presence: true, length: { maximum: 255 },
-  validates :name, presence: true, length: { maximum: 255 },
-  
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password , presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
 end
