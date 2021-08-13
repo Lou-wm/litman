25c6a928591b11615b9292f03b1f722efd5315fe
Lou <2635501727@qq.com>
2021-08-13 19:36:03 +0800

    status_int

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }
   validates :password , presence: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
+  validates :status, inclusion: {in: 0..1 }
 end
