4fe479268b2b7948e31254e8be3cce42c68e5fb8
Lou <2635501727@qq.com>
2021-08-13 20:26:54 +0800

    status_enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }
   validates :password , presence: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
-  validates :status, inclusion: {in: 0..1 }
+  # validates :status, inclusion: {in: 0..1 }
+  validates :status, inclusion: {in: %w(active inactive) }
+   enum status: [ :active, :inactive ]
 end
