15f89d4569bf60f2b8bd10410e9ed1aa49f3660f
Lou <2635501727@qq.com>
2021-08-13 20:36:21 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: "admin@relationshipapp.com", password: "test1234" , name: "litman")
+User.create!(email: "admin@relationshipapp.com", password: "test1234" , name: "litman" ,status: "active")
